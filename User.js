ff1b108b7829ce31c5f3267a47aaef83cc750e37
Mondli <mondli46401@gmail.com>
2025-08-25 10:51:23 +0200

    The working version

--- User.js
+++ User.js
@@ -1,6 +1,4 @@
-//=== Here is my Class ===
 class User {
-    //== Here is my constructor for accessing the class;
     constructor(username, password, email, createdAt, loggedInStat) {
         this.username = username;
         this.password = password;
@@ -9,22 +7,10 @@ class User {
         this.loggedInStat = loggedInStat;
     }
 
-    //== Here are my getters and setters for accessing my data individually if necessary....
     get getUserName() { return this.username }
-    set setUserName(newName) { this.username = newName }
-
     get getPassword() { return this.password }
-    set setPassword(newPass) { this.password = newPass }
-
     get getEmail() { return this.email }
-    set setEmail(newEmail) { this.email = newEmail }
-
     get getCreatedAt() { return this.createdAt }
-    set setCreatedAt(theDate) { this.createdAt = theDate }
-
     get getLoggedStat() { return this.loggedInStat }
-    set setLoggedInStat(stat) { this.loggedInStat = stat }
 }
-
-
 module.exports = User;
--- middleMan.js
+++ middleMan.js
@@ -1,53 +1,68 @@
-//== Here ar all the necessary imports =====
-const User = require(`./User`);
+const express = require("express");
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+const User = require("./User");
 
-
-//=== Here I am creating the Database that we will work with ===
-// This line here I am importing the SQLite3 Library in Node.js
-// The verbose method helps me catch error and be able to see what happening behind the sceen in SQLite3
-const sqlite3 = require('sqlite3').verbose();
-let sql;
-
-// Bellow I am creating a constructor that opens or create the database.
-// The 
-const db = new sqlite3.Database('./theRecords.db',sqlite3.OPEN_READWRITE,(err)=>{
-    if(err) return console.log(err.message);
+const app = express();
+const db = new sqlite3.Database("./theRecords.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+    console.log("Connected to database.");
 });
 
-// Now I will create a table by uncommenting the following code
-// Please uncomment if you don't have a table yet, then run (node middleMan.js) on a terminal to create
-// After running, the table will be there then comment the following code again.
-/*sql = `CREATE TABLE user(id INTEGER PRIMARY KEY, username,password,email,creation_date,logged_in_status)`;
-db.run(sql);*/
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
-// The nect line of code. uncomment it if you want to delete the table
-/* db.run(`DROP TABLE user`);*/
+// === Create table if not exists ===
+db.run(`CREATE TABLE IF NOT EXISTS user(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT,
+    email TEXT,
+    creation_date TEXT,
+    logged_in_status INTEGER
+)`);
 
+// === Show Signup Form ===
+app.get("/", (req, res) => {
+    res.render("signup");
+});
 
+// === Handle Form Submission (FINAL) ===
+// === Handle Form Submission (FINAL) ===
+app.post("/register", (req, res) => {
+    const { username, email, password } = req.body;
+    const createDate = new Date().toISOString();
+    const loggedInStat = 1; // user just signed up, mark as online
 
-//== From the Sign Up Page
-function registerUser(){
-    let userName = document.getElementById("username").value;
-    let userEmail = document.getElementById("email").value;
-    let newPass = document.getElementById("newUserPass").value;
-    let createDate = new Date().toISOString();
-    let loggedInStat = 0;
-
-    if(!userName || !userEmail || !newPass){
-        alert("Please fill in all the fields..!!!");
-        return;
+    if (!username || !email || !password) {
+        return res.send("⚠ Please fill in all fields!");
     }
 
-    //=== Next os the object that will hold the user details
-    const newUser = new User(userName, newPass, userEmail, createDate, loggedInStat);
-    
-    //== Now I will use SQL to insert the users data
-    sql = `INSERT INTO user(username, password, email, creation_date, logged_in_status) VALUES(?,?,?,?,?)`;
-    db.run(sql,[newUser.getUserName(),newUser.getPassword(),newUser.getEmail(),
-               newUser.getCreatedAt(),newUser.getLoggedStat()],
-               (err)=>{if(err) return console.log(err.message)});
+    const newUser = new User(username, password, email, createDate, loggedInStat);
 
-     alert(`New User Successfully Added to the DataBase...`)          
-}
+    const sql = `INSERT INTO user(username, password, email, creation_date, logged_in_status) VALUES(?,?,?,?,?)`;
+    db.run(sql, [newUser.getUserName(), newUser.getPassword, newUser.getEmail, newUser.getCreatedAt, newUser.getLoggedStat],
+        function (err) {
+            if (err) {
+                console.error(err.message);
+                return res.send("Error adding user to database.");
+            }
+
+            // Get the last inserted user ID
+            const userId = this.lastID;
+
+            // Fetch the inserted user to display on profile
+            db.get(`SELECT * FROM user WHERE id = ?`, [userId], (err, row) => {
+                if (err) {
+                    console.error(err.message);
+                    return res.send("Error fetching user profile.");
+                }
+                res.render("profile", { user: row });
+            });
+        });
+});
 
 
+// Start server
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
